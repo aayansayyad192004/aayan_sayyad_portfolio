69c9a73c293eeca22e7173bc9828ebedb0d76b91
aayansayyad192004 <aayansayyad19@gmail.com>
2024-07-27 13:56:16 +0530

    added requirements

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@vercel/speed-insights'], // Exclude problematic package
-    },
-  },
-})
+      external: [
+        '@vercel/speed-insights' // Externalize this module if it's causing issues
+      ]
+    }
+  }
+});
